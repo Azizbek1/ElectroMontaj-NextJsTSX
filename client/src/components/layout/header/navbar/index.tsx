31554b2c5668fed31e0af45daa27fad9585a01a2
Azizbek <46714958+Azizbek1@users.noreply.github.com>
2023-02-03 08:20:02 +0500

    Bug fix

--- client/src/components/layout/header/navbar/index.tsx
+++ client/src/components/layout/header/navbar/index.tsx
@@ -1,27 +1,7 @@
 "use client"
-
-import { useEffect, useState } from "react";
-
-
 export default function Navbar() {
-  const [navSize, setnavSize] = useState("10rem");
-  const [navColor, setnavColor] = useState("transparent");
-  const listenScrollEvent = () => {
-    window.scrollY > 10 ? setnavColor("#252734") : setnavColor("transparent");
-    window.scrollY > 10 ? setnavSize("5rem") : setnavSize("10rem");
-  };
-  useEffect(() => {
-    window.addEventListener("scroll", listenScrollEvent);
-    return () => {
-      window.removeEventListener("scroll", listenScrollEvent);
-    };
-  }, []);
   return (
-    <nav  style={{
-      backgroundColor: navColor,
-      height: navSize,
-      transition: "all 1s"
-    }}>
+    <nav>
       <ul>
         <li>
           <a href="#">Главная</a>
