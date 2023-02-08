a0df4a37fedc68f99dbfef41582c745b26f29c5b
Azizbek1 <azizochilov569@gmail.com>
2023-02-08 11:25:52 +0500

    bu

--- client/app/providers/MainProvider.tsx
+++ client/app/providers/MainProvider.tsx
@@ -1,34 +1,13 @@
-import AuthProvider from './AuthProvider/AuthProvider'
-import HeadProvider from './HeadProvider/HeadProvider'
-import { FC } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { Provider } from 'react-redux'
-import { store } from 'store/store'
+import HeadProvider from "./HeadProvider/HeadProvider";
+import { FC } from "react";
+import Layout from "@/components/layout/Layout";
 
-import Layout from '@/components/layout/Layout'
+const MainProvider: FC = ({ children}) => {
+  return (
+    <HeadProvider>
+      <Layout>{children}</Layout>
+    </HeadProvider>
+  );
+};
 
-import { TypeComponentAuthFields } from '@/shared/types/auth.types'
-
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnWindowFocus: false,
-		},
-	},
-})
-
-const MainProvider: FC<TypeComponentAuthFields> = ({ children, Component }) => {
-	return (
-		<HeadProvider>
-			<Provider store={store}>
-				<QueryClientProvider client={queryClient}>
-					<AuthProvider Component={Component}>
-						<Layout>{children}</Layout>
-					</AuthProvider>
-				</QueryClientProvider>
-			</Provider>
-		</HeadProvider>
-	)
-}
-
-export default MainProvider
+export default MainProvider;
