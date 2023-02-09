93a633c947ed58070540b1fe039c03096ce8a69e
Azizbek1 <azizochilov569@gmail.com>
2023-02-09 15:31:58 +0500

    next

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	poweredByHeader: false,
+	images: {
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+	},
 	env: {
 		APP_URL: process.env.REACT_APP_URL,
 		APP_ENV: process.env.REACT_APP_ENV,
