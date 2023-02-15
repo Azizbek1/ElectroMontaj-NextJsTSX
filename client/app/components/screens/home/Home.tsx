2790167d3481d33952d619f8c0b6b5fcf0d7987d
Azizbek1 <azizochilov569@gmail.com>
2023-02-15 13:46:40 +0500

    Modal

--- client/app/components/screens/home/Home.tsx
+++ client/app/components/screens/home/Home.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Meta } from "@/utils/meta";
 import Servises from "./components/Servises/Servises";
 import Dover from "./components/Dover/Dover";
@@ -7,10 +7,28 @@ import Price from "./components/Price/Price";
 import About from "./components/About/About";
 import News from "./components/News/News";
 import Slider from "./components/Slider/Slider";
+import Modal from "@/components/ui/modal/Modal";
+import {mockResponse as response, Transaction} from "@/components/ui/modal/data";
 
 const Home: FC = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [
+    currentTransaction,
+    setCurrentTransaction
+  ] = useState<Transaction | null>(null);
+
+  const openModal = (transaction: Transaction) => {
+    setModalOpen(true);
+    setCurrentTransaction(transaction);
+  };
+
+  useEffect(() => {
+    setTransactions(response.data.transactions);
+  }, []);
   return (
     <Meta title="Электро-монтаж" description="">
+    
       <Slider />
       <Servises />
       <Dover />
@@ -18,6 +36,13 @@ const Home: FC = () => {
       <Price />
       <About />
       <News />
+      <Modal
+        isOpen={modalOpen}
+        transaction={currentTransaction}
+        closeModal={() => {
+          setModalOpen(false);
+        }}
+      />
     </Meta>
   );
 };
--- client/app/components/ui/modal/Modal.tsx
+++ client/app/components/ui/modal/Modal.tsx
@@ -1,2 +1,38 @@
-export {
+import ReactModal from "react-modal";
+import { Transaction } from "./data";
+
+interface ModalProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  transaction?: Transaction | null;
+}
+
+export default function Modal({
+  isOpen,
+  closeModal,
+  transaction
+}: ModalProps): JSX.Element {
+  const renderTransaction = (transaction: Transaction) => {
+    return (
+      <div>
+        <p>{`Transaction for ${transaction.entityName}`}</p>
+        <p>{`${transaction.status} for ${transaction.category} on ${transaction.transactionDate}`}</p>
+        <small>{transaction.id}</small>
+      </div>
+    );
+  };
+
+  return (
+    <ReactModal isOpen={isOpen} onRequestClose={closeModal} ariaHideApp={false}>
+      <div className="modal-header">
+        <button onClick={closeModal}>x</button>
+      </div>
+
+      {transaction ? (
+        renderTransaction(transaction)
+      ) : (
+        <p>No available transaction.</p>
+      )}
+    </ReactModal>
+  );
 }
