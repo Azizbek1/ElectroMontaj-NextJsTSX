08ae9cf9d8cdaab30fa4c4b2496b2ba9caaaf157
Azizbek1 <azizochilov569@gmail.com>
2023-02-15 10:44:48 +0500

    footer

--- client/app/components/fakeData/footer/footerData.ts
+++ client/app/components/fakeData/footer/footerData.ts
@@ -1,3 +1,27 @@
-export {
-    
+import { IFooterAdress } from "./footer.props";
+
+
+export const menus: IFooterAdress = {
+    items: [
+        {
+            id: 1,
+            icon: 'MdPhoneForwarded',
+            title: 'Главная',
+        },
+        {
+            id: 2,
+            icon: 'MdPhoneForwarded',
+            title: 'О Компании',
+        },
+        {
+            id: 3,
+            icon: 'MdPhoneForwarded',
+            title: 'Услуги',
+        },
+        {
+            id: 4,
+            icon: 'MdPhoneForwarded',
+            title: 'Новости',
+        }
+    ],
 }
