dfc8190bab17a2e34fc55d7b8c19c324499c43d8
Azizbek <46714958+Azizbek1@users.noreply.github.com>
2023-02-18 17:18:08 +0500

    ss

--- client/app/components/screens/about/components/AboutServises/AboutServises.item.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AboutServisesItem() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- client/app/components/screens/about/components/AboutServises/AboutServises.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AboutServises() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
