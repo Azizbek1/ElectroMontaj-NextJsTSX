0024d586c7a5f4a570d529fbba6745493ef4588b
Azizbek <46714958+Azizbek1@users.noreply.github.com>
2023-03-04 22:51:06 +0500

    Bug

--- clientAdmin2Version/src/services/admin/admin.service.ts
+++
@@ -1,9 +0,0 @@
-import { getUsersUrl } from '@/configs/api.config'
-import axios from 'api/interceptors'
-
-
-export const AdminService = {
-	async getCountUsers() {
-		return axios.get<number>(getUsersUrl('/count'))
-	},
-}
---
+++ clientAdmin2Version/src/types/auth.types.ts
@@ -0,0 +1,6 @@
+
+export type TypeRoles = { isOnlyAdmin?: boolean; isOnlyUser?: boolean }
+
+export type NextPageAuth = TypeRoles
+
+export type TypeComponentAuthFields = { Component: TypeRoles }
